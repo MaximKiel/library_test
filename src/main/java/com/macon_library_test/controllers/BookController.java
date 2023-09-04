039542f76ccd6f363dee762c03214f39d0f211d3
MaximKiel <loup.du.nord@yandex.ru>
2023-09-04 21:58:22 +0300

    Implement BookController.download

--- src/main/java/com/macon_library_test/controllers/BookController.java
+++ src/main/java/com/macon_library_test/controllers/BookController.java
@@ -3,12 +3,19 @@ package com.macon_library_test.controllers;
 import com.macon_library_test.dao.BookDAO;
 import com.macon_library_test.model.Book;
 import com.macon_library_test.utul.SearchBook;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/library/books")
@@ -84,5 +91,21 @@ public class BookController {
     public String delete(@PathVariable("id") int id) {
         bookDAO.delete(id);
         return "redirect:/library/books";
+    }
+
+//  https://sky.pro/media/zagruzka-fajla-iz-kontrollerov-spring/
+    @GetMapping("/{id}/download")
+    public ResponseEntity<byte[]> download(@PathVariable("id") int id) {
+        byte[] pdfData;
+        try {
+            pdfData = Files.readAllBytes(Paths.get("C:\\Users\\79883\\MyJava\\macon\\library_test\\src\\main\\" +
+                    "webapp\\WEB-INF\\views\\library\\books\\pdf\\"+bookDAO.show(id).getTitle()+".pdf"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData(bookDAO.show(id).getTitle(), bookDAO.show(id).getTitle());
+        return new ResponseEntity<>(pdfData, headers, HttpStatus.OK);
     }
 }
